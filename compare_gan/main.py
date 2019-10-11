184d8d04d3365cdfb2fd813f398ebaf64fa25f15
frl <frl1996@mail.ustc.edu.cn>
2019-10-11 20:20:46 +0800

    's'

--- compare_gan/main.py
+++ compare_gan/main.py
@@ -88,7 +88,7 @@ def _get_run_config(tf_random_seed=None,
   tpu_config = tf.contrib.tpu.TPUConfig(
       num_shards=1 if single_core else None,  # None = all cores.
       iterations_per_loop=iterations_per_loop)
-  strategy= tf.contrib.distribute.MirroredStrategy(num_gpus=8)
+  strategy = tf.distribute.MirroredStrategy(devices=[0,1,2,3,4,5,6,7])
   return tf.contrib.tpu.RunConfig(
       model_dir=FLAGS.model_dir,
       eval_distribute=strategy,
